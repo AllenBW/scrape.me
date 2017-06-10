a2c5d742155567d4ceecb1e64492718d6aeffcf8
Allen Wight <awight@redhat.com>
2017-06-09 21:54:32 -0400

    Plants vue seed, slaps on some custom paint, adds a few treats
    Adds pre-commit hooks, sass, standard, and a few fun deps for the future

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="sass">
+  @import '../node_modules/bootstrap/dist/css/bootstrap'
+  @import '../node_modules/bootstrap-vue/dist/bootstrap-vue'
+
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+</style>
